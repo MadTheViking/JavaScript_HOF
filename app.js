9f5386a10599f6d2038c7403db2ef5c7f96428f9
David Truesdale <David8872Truesdale@hotmail.com>
2022-06-14 13:43:44 -0500

    update

--- app.js
+++ app.js
@@ -1,4 +1,62 @@
 console.log("Hello World!\n==========\n");
 
-// Exercise 1 Section
+function plus(num){
+   return function (){
+        return num + plusNumber();
+    };
+}
+
+//const plus2 = plus(2);
+
+//console.log(plus2(2));
+//console.log(plus2(100));
+
 console.log("EXERCISE 1:\n==========\n");
+
+let users = [
+    {
+      name: "Frodo",
+      age: 50,
+      score: 85,
+      isActive: false,
+    },
+    {
+      name: "Sam",
+      age: 38,
+      score: 94,
+      isActive: true,
+    },
+    {
+      name: "Merry",
+      age: 36,
+      score: 82,
+      isActive: true,
+    },
+    {
+      name: "Pippin",
+      age: 26,
+      score: 77,
+      isActive: false,
+    },
+  ];
+
+
+  users.forEach(user => console.log(user.name))
+
+  //exercise3
+
+
+let newUser = users.map((user) => {
+    return { name: user.name, score: user.score };
+});
+console.log(newUser)
+
+//escierse 4
+
+//let activeUsers = users.filiter((user) => user.isActive);
+
+//console.log(activeUsers);
+users.sort((a, b) => b.score - a.score);
+
+
+console.log(users)
